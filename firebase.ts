4be55e073752ce4013251f685192ac6235bd3a73
jugalipo <jugalipo@gmail.com>
2026-03-15 09:14:08 +0100

    Add files via upload

--- firebase.ts
+++ firebase.ts
@@ -5,12 +5,12 @@ import firebaseConfig from './firebase-applet-config.json';
 
 // Initialize Firebase SDK
 const app = initializeApp({
-  apiKey: process.env.VITE_FIREBASE_API_KEY || firebaseConfig.apiKey,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN || firebaseConfig.authDomain,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID || firebaseConfig.projectId,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET || firebaseConfig.storageBucket,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID || firebaseConfig.messagingSenderId,
-  appId: process.env.VITE_FIREBASE_APP_ID || firebaseConfig.appId,
+  apiKey: "AIzaSyBLnNjuQNoC1E4hSnhlrXzGl2pVMPHf_YA",
+  authDomain: "el-reino-354ca.firebaseapp.com",
+  projectId: "el-reino-354ca",
+  storageBucket: "el-reino-354ca.firebasestorage.app",
+  messagingSenderId: "504444808693",
+  appId: "1:504444808693:web:97ca0afbbf428d0cc344bc"
 });
 
 // Initialize Firestore with offline persistence
